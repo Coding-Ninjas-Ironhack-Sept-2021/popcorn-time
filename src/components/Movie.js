79de990a3fe9e804db706125070aa169f7a178c8
Luis Junco <38411763+ludovico-wd@users.noreply.github.com>
2021-11-03 16:57:37 +0100

    conditional rendering: option 1 (using variable)

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,11 +1,18 @@
 import React from "react";
 
 function Movie(props){
+
+    let badge;
+    if(props.rating > 8) {
+        badge = <p className="badge"><strong>POPULAR</strong></p>
+    }
+
     return (
         <div className="movie">
             <h2>{props.title}</h2>
             <p>Year: {props.year}</p>
             <p>Rating: {props.rating}</p>
+            {badge}
         </div>
     );
 }
