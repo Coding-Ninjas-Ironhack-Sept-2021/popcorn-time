1cc879e76aebe3cdf5c49fe18bfdf1877e401916
Luis Junco <38411763+ludovico-wd@users.noreply.github.com>
2021-11-03 17:17:35 +0100

    conditional rendering: option 4 (with a helper function)

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,18 +1,21 @@
 import React from "react";
 
-function Movie(props){
 
+function renderBadge(movieRating){
+    if(movieRating > 8){
+        return <p className="badge"><strong>POPULAR</strong></p>
+    }
+}
+
+function Movie(props){
+    
     return (
         <div className="movie">
             <h2>{props.title}</h2>
             <p>Year: {props.year}</p>
             <p>Rating: {props.rating}</p>
 
-            { 
-                props.rating > 8 ?
-                <p className="badge"><strong>POPULAR</strong></p> : 
-                "" 
-            }
+            { renderBadge(props.rating) }
 
         </div>
     );
